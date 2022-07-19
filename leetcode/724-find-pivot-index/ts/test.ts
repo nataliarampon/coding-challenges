bcc37c110dd455971a6ae662ead0cee1426d3f3f
nataliarampon <nataliarampon@gmail.com>
2022-07-18 21:24:24 -0300

    test: create test cases for leetcode #724

---
+++ leetcode/724-find-pivot-index/ts/test.ts
@@ -0,0 +1,29 @@
+import { expect } from "chai";
+import "mocha";
+import { solution } from "./solution";
+
+describe("Running sum of 1D array", () => {
+    it("should pass test case #1", () => {
+        const input:Array<number> = [1,7,3,6,5,6];
+        const expectedResult = 3;
+
+        const result:number = solution(input);
+        expect(result).to.equal(expectedResult);
+    });
+
+    it("should pass test case #2", () => {
+        const input:Array<number> = [1,2,3];
+        const expectedResult = -1;
+
+        const result:number = solution(input);
+        expect(result).to.equal(expectedResult);
+    });
+
+    it("should pass test case #3", () => {
+        const input:Array<number> = [2,1,-1];
+        const expectedResult = 0;
+
+        const result:number = solution(input);
+        expect(result).to.equal(expectedResult);
+    });
+});
