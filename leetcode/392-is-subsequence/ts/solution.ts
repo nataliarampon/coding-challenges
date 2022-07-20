9ae96d73a439d81ed04b8efa8c79d590e2bc4a53
nataliarampon <nataliarampon@gmail.com>
2022-07-19 21:24:39 -0300

    feat: add dynamic programming solution to lc #392

--- leetcode/392-is-subsequence/ts/solution.ts
+++ leetcode/392-is-subsequence/ts/solution.ts
@@ -15,3 +15,40 @@ export function solution(subsequence: string, originalString: string): boolean {
 
     return i == subsequence.length;
 }
+
+export function dynamicProgrammingSolution(subsequence: string, originalString: string): boolean {
+    // handle empty strings (it's either this or using conditionals)
+    subsequence = "X" + subsequence;
+    originalString = "X" + originalString;
+
+    const matrix:boolean[][] = new Array(subsequence.length).fill(false).map(() => new Array(originalString.length).fill(false));
+    const subLength:number = subsequence.length;
+    const oriLength:number = originalString.length;
+
+    if (subLength > oriLength) {
+        return false;
+    }
+    
+    for (let i = 0; i < subLength; i++) {
+        for (let j = i; j < oriLength; j++) {
+            if (subsequence[i] == originalString[j]) {
+                if (i == 0 || j == 0) {
+                    matrix[i][j] = true;
+                } else {
+                    // if a new character matches, it will be a match if the previous character on the subsequence matched up to one character before on the string
+                    matrix[i][j] = matrix[i-1][j-1];
+                }
+            } else {
+                if (j == 0) {
+                    matrix[i][j] = false;
+                } else {
+                    // if no match happens, the new subsequence will still match if it previously matched to the character before on the string
+                    // if it did not match anything new, it matters not if the subsequence (i) matched before or not, because we are not furthering our knowledge
+                    matrix[i][j] = matrix[i][j-1];
+                }
+            }
+        }
+    }
+
+    return matrix[subLength-1][oriLength-1];
+}
--- leetcode/392-is-subsequence/ts/test.ts
+++ leetcode/392-is-subsequence/ts/test.ts
@@ -1,86 +1,171 @@
 import { expect } from "chai";
 import "mocha";
-import { solution } from "./solution";
+import { dynamicProgrammingSolution, solution } from "./solution";
 
 describe("Is subsequence", () => {
-    it("should pass test case #1", () => {
-        const subsequence = "abc";
-        const originalString = "ahbgdc";
-        const expectedResult = true;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
+    describe("2 pointers approach", () => {
+        it("should pass test case #1", () => {
+            const subsequence = "abc";
+            const originalString = "ahbgdc";
+            const expectedResult = true;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #2", () => {
+            const subsequence = "axc";
+            const originalString = "ahbgdc";
+            const expectedResult = false;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #3", () => {
+            const subsequence = "ac";
+            const originalString = "ahbgdcccccc";
+            const expectedResult = true;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #4", () => {
+            const subsequence = "ace";
+            const originalString = "abcde";
+            const expectedResult = true;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #5", () => {
+            const subsequence = "aec";
+            const originalString = "abcde";
+            const expectedResult = false;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass subsequence longer than string", () => {
+            const subsequence = "aecccccccccc";
+            const originalString = "abcde";
+            const expectedResult = false;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass same string test case", () => {
+            const subsequence = "abc";
+            const originalString = "abc";
+            const expectedResult = true;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass trivial test case", () => {
+            const subsequence = "";
+            const originalString = "";
+            const expectedResult = true;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass low-length test case", () => {
+            const subsequence = "a";
+            const originalString = "b";
+            const expectedResult = false;
+    
+            const result:boolean = solution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
     });
 
-    it("should pass test case #2", () => {
-        const subsequence = "axc";
-        const originalString = "ahbgdc";
-        const expectedResult = false;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass test case #3", () => {
-        const subsequence = "ac";
-        const originalString = "ahbgdcccccc";
-        const expectedResult = true;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass test case #4", () => {
-        const subsequence = "ace";
-        const originalString = "abcde";
-        const expectedResult = true;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass test case #5", () => {
-        const subsequence = "aec";
-        const originalString = "abcde";
-        const expectedResult = false;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass subsequence longer than string", () => {
-        const subsequence = "aecccccccccc";
-        const originalString = "abcde";
-        const expectedResult = false;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass same string test case", () => {
-        const subsequence = "abc";
-        const originalString = "abc";
-        const expectedResult = true;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass trivial test case", () => {
-        const subsequence = "";
-        const originalString = "";
-        const expectedResult = true;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
-    });
-
-    it("should pass low-length test case", () => {
-        const subsequence = "a";
-        const originalString = "b";
-        const expectedResult = false;
-
-        const result:boolean = solution(subsequence, originalString);
-        expect(result).to.equal(expectedResult);
+    describe("Dynamic programming approach", () => {
+        it("should pass test case #1", () => {
+            const subsequence = "abc";
+            const originalString = "ahbgdc";
+            const expectedResult = true;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #2", () => {
+            const subsequence = "axc";
+            const originalString = "ahbgdc";
+            const expectedResult = false;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #3", () => {
+            const subsequence = "ac";
+            const originalString = "ahbgdcccccc";
+            const expectedResult = true;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #4", () => {
+            const subsequence = "ace";
+            const originalString = "abcde";
+            const expectedResult = true;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass test case #5", () => {
+            const subsequence = "aec";
+            const originalString = "abcde";
+            const expectedResult = false;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass subsequence longer than string", () => {
+            const subsequence = "aecccccccccc";
+            const originalString = "abcde";
+            const expectedResult = false;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass same string test case", () => {
+            const subsequence = "abc";
+            const originalString = "abc";
+            const expectedResult = true;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass trivial test case", () => {
+            const subsequence = "";
+            const originalString = "";
+            const expectedResult = true;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
+    
+        it("should pass low-length test case", () => {
+            const subsequence = "a";
+            const originalString = "b";
+            const expectedResult = false;
+    
+            const result:boolean = dynamicProgrammingSolution(subsequence, originalString);
+            expect(result).to.equal(expectedResult);
+        });
     });
 });
