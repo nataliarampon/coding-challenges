5ff312921c511ce3b34eb74c28db5ab6fc3b7d64
nataliarampon <nataliarampon@gmail.com>
2022-07-20 23:41:59 -0300

    refactor: simpler solution with explanation

--- leetcode/206-reverse-linked-list/ts/solution.ts
+++ leetcode/206-reverse-linked-list/ts/solution.ts
@@ -1,11 +1,31 @@
 import { ListNode } from "../../util/list/list";
 
-export function solution(input: ListNode | null): ListNode | null {
-    if (input != null && input.next != null) {
-        const reversed = solution(input.next);
-        input.next.next = input;
-        input.next = null;
-        return reversed;
+export function solution(head: ListNode | null, previous: ListNode | null = null): ListNode | null {
+    let current: ListNode | null = null;
+
+    if (head == null) {
+        return previous;
     }
-    return input;
+
+    /**
+     * This solution is similar to having three pointers. Explaining with the following list as an example: A -> B -> C -> null
+     * There's the head pointer, which initially points to A. And each time we're processing things, we need to keep track of
+     * the rest of the list to the right, since we override the linking on A. 
+     * Thus our current pointer will start pointing to B first thing, to save the rest of the list. So now we have:
+     * A (head) -> B (current) -> C -> null     || null (previous)
+     */
+    current = head.next;
+    /**
+     * Now, we need to make the current element point to the previous one, to reverse the list.
+     * So now we have:
+     * A (head) -> null (previous)    ||      B (current) -> C -> null
+     * Notice how we'd have lost access to B if we hadn't put it into the current pointer.
+     */
+    head.next = previous;
+    /**
+     * The next call will have the following initial setup (notice how the argument previous serves as a third pointer):
+     * A (previous) -> null    ||      B (head) -> C -> null
+     * This way, in the next call, we will be moving the current pointer to C, and making B point to A, reversing the list.
+     */
+    return solution(current, head);
 }
