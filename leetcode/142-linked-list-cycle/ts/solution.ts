281683540da20a89053ba13087de23561dfd3574
nataliarampon <nataliarampon@gmail.com>
2022-07-21 22:03:13 -0300

    feat: add solution to LC #142

--- leetcode/142-linked-list-cycle/ts/solution.ts
+++ leetcode/142-linked-list-cycle/ts/solution.ts
@@ -1,5 +1,17 @@
+import { Set } from "typescript";
 import { ListNode } from "../../util/list/list";
 
 export function detectCycle(head: ListNode | null): ListNode | null {
-    return null;
+    const visitedNodes: Set<ListNode | void> = new Set();
+
+    while (head) {
+        if (visitedNodes.has(head)) {
+            return head;
+        } else {
+            visitedNodes.add(head);
+        }
+        head = head.next;
+    }
+
+    return head;
 }
