1901f3ad7c81f4d6f97d01ee02bf6a3dab21b1d7
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-06 21:43:35 -0300

    feat: add solution for BC #2756

---
+++ beecrowd/2756-output-10/py/main.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+  STRING_A = 7*' ' + 'A'
+  STRING_B = 6*' ' + 'B' + ' ' + 'B'
+  STRING_C = 5*' ' + 'C' + 3* ' ' + 'C'
+  STRING_D = 4*' ' + 'D' + 5*' ' + 'D'
+  STRING_E = 3*' ' + 'E' + 7*' ' + 'E'
+
+  print(STRING_A)
+  print(STRING_B)
+  print(STRING_C)
+  print(STRING_D)
+  print(STRING_E)
+  print(STRING_D)
+  print(STRING_C)
+  print(STRING_B)
+  print(STRING_A)
