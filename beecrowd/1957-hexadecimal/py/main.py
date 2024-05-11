8392f9e87dbeb11855977b2613aa72f34692a67f
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-11 08:58:34 -0300

    feat: add solution for BC #1957

---
+++ beecrowd/1957-hexadecimal/py/main.py
@@ -0,0 +1,7 @@
+def main():
+  decimal = int(input())
+  hexa = str(hex(decimal))[2:].upper()
+  print(hexa)                                                                                                                                                                                                                                                                                                                                    
+
+if __name__ == '__main__':
+  main()
