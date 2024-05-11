964a06865a45718e969b601f7edcf4249ce00993
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-11 12:52:11 -0300

    feat: add solution for BC #2483

---
+++ beecrowd/2483-merry-christmas/py/main.py
@@ -0,0 +1,6 @@
+def main():
+  repetition = int(input())
+  print('Feliz nat{}l!'.format('a'*repetition))                                                                                                                                                                                                                                                                                                                                 
+
+if __name__ == '__main__':
+  main()
