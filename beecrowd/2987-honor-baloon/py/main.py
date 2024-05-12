1aeffc522ab0a7f2791182082d0211ac1fca3818
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-12 10:55:10 -0300

    feat: add solution for BC #2987

---
+++ beecrowd/2987-honor-baloon/py/main.py
@@ -0,0 +1,8 @@
+import string
+
+def main():
+  letter = input()
+  print(string.ascii_uppercase.index(letter) + 1)                                                                                                                                                                                                                                                                                                                           
+
+if __name__ == '__main__':
+  main()
