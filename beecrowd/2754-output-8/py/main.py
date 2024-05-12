581ec0781366a64b1c7f6235d2c8fca1aed2b456
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-12 20:00:03 -0300

    feat: add solution for BC #2754

---
+++ beecrowd/2754-output-8/py/main.py
@@ -0,0 +1,16 @@
+def main():
+  a = 234.345
+  b = 45.698
+
+  print(f'{a:.6f} - {b:.6f}')
+  print(f'{a:.0f} - {b:.0f}')
+  print(f'{a:.1f} - {b:.1f}')
+  print(f'{a:.2f} - {b:.2f}')
+  print(f'{a:.3f} - {b:.3f}')
+  print(f'{a:e} - {b:e}')
+  print(f'{a:E} - {b:E}')
+  print(f'{a:.3f} - {b:.3f}')
+  print(f'{a:.3f} - {b:.3f}')
+
+if __name__ == '__main__':
+  main()
