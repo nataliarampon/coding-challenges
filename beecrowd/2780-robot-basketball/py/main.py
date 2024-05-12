25c1a6a150f7f7749e93ffae9ce948a6c35e8b79
Natália Gubiani Rampon <ngrampon@inf.ufrgs.br>
2024-05-12 10:52:34 -0300

    feat: add solution for BC #2780

---
+++ beecrowd/2780-robot-basketball/py/main.py
@@ -0,0 +1,11 @@
+def main():
+  distance = int(input())
+  if distance <= 800:
+    print(1)
+  elif distance <= 1400:
+    print(2)
+  else:
+    print(3)                                                                                                                                                                                                                                                                                                                              
+
+if __name__ == '__main__':
+  main()
