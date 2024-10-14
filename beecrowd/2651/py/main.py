7b69aa028164aab557167482ffc13bd2716b0bac
Natália Rampon <ngrampon@inf.ufrgs.br>
2024-10-14 16:41:01 -0300

    feat: add solution for BC #2651

---
+++ beecrowd/2651/py/main.py
@@ -0,0 +1,9 @@
+def main():
+    sentence = input().lower()
+    if 'zelda' in sentence:
+        print('Link Bolado')
+    else:
+        print('Link Tranquilo')
+
+if __name__ == "__main__":
+    main()
