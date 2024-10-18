948e2bd905a875b1c2699569ab18380cf6d937df
Natália Rampon <ngrampon@inf.ufrgs.br>
2024-10-18 14:09:01 -0300

    feat: fix solution for BC #1023

--- beecrowd/1023-drought/py/main.py
+++ beecrowd/1023-drought/py/main.py
@@ -1,44 +1,50 @@
-# -*- coding: utf-8 -*-
+# This is giving time limit exceeded
 
 import math
+from sys import stdin, stdout
 
 class Property:
     def __init__(self, residents = 0, consumption = 0) -> None:
         self.residents = residents
         self.consumption = consumption
+        self.avg = 0
     
     @staticmethod
     def printCityStatistics(properties, city_number):
-        print("Cidade# {}:".format(city_number))
+        stdout.write("Cidade# {}:\n".format(city_number))
 
         property_stats = ""
         sum_consumption = 0
         sum_residents = 0
 
         for property in properties:
-            property_stats += "{}-{} ".format(property.residents, property.consumption//property.residents)
+            property_stats += "{}-{} ".format(property.residents, property.avg)
             sum_consumption += property.consumption
             sum_residents += property.residents
         property_stats = property_stats[:-1] + '\n'
 
-        print(property_stats, end='')
-        print("Consumo medio: {:.2f} m3.".format(math.floor(sum_consumption/sum_residents)))
+        stdout.write(property_stats)
+        stdout.write("Consumo medio: {:.2f} m3.\n".format(math.floor(sum_consumption/sum_residents * 100)/100.0))
 
 city_number = 1
 
 while True:
-    nb_properties = int(input())
+    nb_properties = int(stdin.readline())
 
     if nb_properties < 1:
-        break;
+        break
+    
+    if city_number > 1:
+        stdout.write('\n')
 
     properties = []
 
     for i in range(nb_properties):
         property = Property()
-        property.residents, property.consumption  = map(int, input().split())
+        property.residents, property.consumption  = map(int, stdin.readline().split())
+        property.avg = property.residents // property.consumption
         properties.append(property)
-    properties.sort(key=lambda property: property.consumption//property.residents)
+    properties.sort(key=lambda property: property.avg)
     
     Property.printCityStatistics(properties, city_number)
 
